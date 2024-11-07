cee674380de0fd75f0807f15dfdf6855a3afe718
Rijad Halili <rijadhalili.yt@gmail.com>
2024-11-07 13:27:48 +0100

    seed to database

--- ExpenseTracker/ExpenseTracker.API/Migrations/20241107122541_seed for dif reg.Designer.cs
+++ ExpenseTracker/ExpenseTracker.API/Migrations/20241107122541_seed for dif reg.Designer.cs
@@ -4,6 +4,7 @@ using ExpenseTracker.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpenseTracker.API.Migrations
 {
     [DbContext(typeof(ExpenseTrackerDbContext))]
-    partial class ExpenseTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241107122541_seed for dif reg")]
+    partial class seedfordifreg
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
