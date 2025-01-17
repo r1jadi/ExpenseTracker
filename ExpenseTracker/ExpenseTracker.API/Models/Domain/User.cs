87b1165e34099fe4845a4a28a0516bb022e44f07
Rijad Halili <rijadhalili.yt@gmail.com>
2025-01-17 18:01:39 +0100

    z

--- ExpenseTracker/ExpenseTracker.API/Models/Domain/User.cs
+++ ExpenseTracker/ExpenseTracker.API/Models/Domain/User.cs
@@ -9,7 +9,6 @@
         public string Password { get; set; }
         public string Role { get; set; }
         public string? ProfilePicture { get; set; }
-
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
