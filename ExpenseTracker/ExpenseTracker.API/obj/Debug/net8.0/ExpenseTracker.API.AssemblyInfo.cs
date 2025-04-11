09cb966fc6ef14aee20e44111e0ca774ca2b0183
Rijad Halili <rijadhalili.yt@gmail.com>
2025-04-11 15:54:50 +0200

    x

--- ExpenseTracker/ExpenseTracker.API/obj/Debug/net8.0/ExpenseTracker.API.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker.API/obj/Debug/net8.0/ExpenseTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0fd9d64421c9a27926ce10c565a2c526e393d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356cdb1858287410c6ae20a1dcc1cb44e9e180de")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
